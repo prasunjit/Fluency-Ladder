51e5dd7a54e0316be9364d8d80ce64b1f0b30bb4
Prasunjit <prasunite@gmail.com>
2016-05-14 14:06:45 -0500

    Added the instructions page for the next level

--- app/src/main/java/edu/nwmissouri/fluencyladder/Instructions.java
+++ app/src/main/java/edu/nwmissouri/fluencyladder/Instructions.java
@@ -5,17 +5,15 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
-public class HomeActivity extends AppCompatActivity {
+public class Instructions extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_instructions);
     }
-
-
-    public void gotoInstructionActivity(View view) {
-        Intent intent1 = new Intent(this, Instructions.class);
+    /*public void gotoFirstActivity(View view) {
+        Intent intent1 = new Intent(this, MainActivity.class);
         startActivity(intent1);
-    }
+    }*/
 }
